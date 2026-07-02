98d035fc99c45f6f2de53c983ddd284e2651c7f6
HectorDaniell <hector.oviedo1312@gmail.com>
2026-07-02 00:33:17 -0500

    feat(documents): add package barrel

---
+++ packages/documents/src/index.ts
@@ -0,0 +1,7 @@
+/**
+ * Superficie pública de @jobfinder/documents.
+ *
+ * El adapter es la única pieza que consume el mundo exterior (la API); el resto
+ * (mappers, exporters, modelos) son detalles internos del package.
+ */
+export { DocumentAdapter } from './adapter';
